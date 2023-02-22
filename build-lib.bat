4b006be0376edaf7ea69454e389ac4a774485086
Gabriel Seaver <gabrielseaver9678@gmail.com>
2023-02-22 12:25:12 -0500

    modified the build process for using vendor libraries rather than the claw.jar

--- build-lib.bat
+++ build-lib.bat
@@ -2,10 +2,11 @@
 
 echo Building CLAW
 
-@REM Build the distribution zip (used for the driverstation client)
-@REM as well as the library "claw.jar" (used for WPILib projects)
 cd lib
+@REM Build the distribution zip (used for the driverstation client)
 call gradlew.bat distZip
+@REM Build the maven repo (used for the test-bot and for any vendor dependencies)
+call gradlew.bat publish
 cd ..
 
 echo Extracting CLAW distribution for driverstation RCT client
@@ -14,16 +15,3 @@ echo Extracting CLAW distribution for driverstation RCT client
 rmdir /S /Q driverstation-app-extract\app
 powershell Expand-Archive lib\app\build\distributions\driverstation-rct-client.zip -DestinationPath driverstation-app-extract
 rename driverstation-app-extract\driverstation-rct-client app
-
-echo Copying claw.jar to test robot
-
-@REM Add the new claw.jar to the test robot
-xcopy lib\app\build\libs\claw.jar test-bot\libs\ /y
-
-echo.
-echo NOTICE
-echo Use the command Clean Java Language Server Workspace if any changes in CLAW are not reflected in the test robot code,
-echo or add/remove a space from the test robot build.gradle and save in order to trigger a classpath recompilation (see
-echo the bottom right corner).
-echo.
-echo.
