0c38786dc325d03c8cc317a01744411847dbdb3a
Gabriel Seaver <gabrielseaver9678@gmail.com>
2023-02-28 09:55:03 -0500

    finalization of subsystem testing command-line utility

--- lib/app/src/main/java/claw/subsystems/CLAWSubsystem.java
+++ lib/app/src/main/java/claw/subsystems/CLAWSubsystem.java
@@ -15,8 +15,9 @@ public abstract class CLAWSubsystem extends SubsystemBase {
     
     public static final CommandProcessor COMMAND_PROCESSOR = new CommandProcessor(
         "subsystem",
-        "subsystem",
-        "subsystem",
+        "subsystem [ list | inspect | test]",
+        "Use 'subsystem list' to list all subsystems. 'subsystem inspect NAME' will retrieve the available tests for a given " +
+        "subsystem. 'subsystem test SUBNAME TESTNAME' will run a test named TESTNAME on the given subsystem SUBNAME.",
         CLAWSubsystem::subsystemCommand
     );
     
@@ -34,9 +35,12 @@ public abstract class CLAWSubsystem extends SubsystemBase {
     private static void subsystemCommand (ConsoleManager console, CommandReader reader) throws BadCallException {
         reader.allowNoOptions();
         reader.allowNoFlags();
-        String operation = reader.readArgOneOf("operation", "Expected 'list', 'inspect'.", "list", "inspect");
+        String operation = reader.readArgOneOf("operation", "Expected 'list', 'inspect' or 'test'.", "list", "inspect", "test");
         
         if (operation.equals("list")) {
+            
+            // List all the instantiated subsystems
+            
             reader.noMoreArgs();
             
             synchronized (subsystems) {
@@ -45,44 +49,58 @@ public abstract class CLAWSubsystem extends SubsystemBase {
                     console.println("There are no instantiated CLAWSubsystems.");
             }
             
-        } else if (operation.equals("inspect")) {
+        } else {
             
+            // Operations which are performed on a specific subsystem
+            
+            // Get the CLAWSubsystem
+            CLAWSubsystem subsystem;
             synchronized (subsystems) {
                 // Get the subsystem name
                 String subsystemName = reader.readArgOneOf("subsystem name", "Expected the name of an exsiting subsystem.", subsystems.keySet());
+                
+                // Get the subsystem and print its supported tests
+                subsystem = subsystems.get(subsystemName);
+            }
+            
+            // Switch based on operation
+            if (operation.equals("inspect")) {
+                
                 reader.noMoreArgs();
                 
-                // Get the subsystem and run inspect
-                CLAWSubsystem subsystem = subsystems.get(subsystemName);
-                subsystemInspect(console, subsystem);
+                // Show available tests for the subsystem
+                if (subsystem.subsystemTests.size() == 0) {
+                    console.println(subsystem.getName()+" supports no tests.");
+                } else {
+                    console.println(subsystem.getName()+" supports the following tests:");
+                    synchronized (subsystem.subsystemTests) {
+                        subsystem.subsystemTests.keySet().forEach(console::println);
+                    }
+                }
+                
+            } else if (operation.equals("test")) {
+                
+                // Run a subsystem test
+                
+                // Get the test specified by the command
+                SubsystemTest test;
+                synchronized (subsystem.subsystemTests) {
+                    // Get a test name to run
+                    Set<String> testNames = subsystem.subsystemTests.keySet();
+                    String testName = reader.readArgOneOf(
+                        "test name",
+                        "Expected a test name from the set of tests supported by the subsystem.",
+                        testNames
+                    );
+                    reader.noMoreArgs();
+                    
+                    test = subsystem.subsystemTests.get(testName);
+                }
+                
+                // Run the subsystem test
+                test.run(console, subsystem);
+                
             }
-            
-        }
-        
-    }
-    
-    private static void subsystemInspect (ConsoleManager console, CLAWSubsystem subsystem) {
-        
-        synchronized (subsystem.subsystemTests) {
-            
-            Set<String> testNames = subsystem.subsystemTests.keySet();
-            if (testNames.size() == 0) {
-                console.println("Subsystem has no tests.");
-            } else {
-                console.println("Subsystem tests:");
-                testNames.forEach(console::println);
-            }
-            
-        }
-        
-        console.println("Use 'test' to run a test with a given name, or 'quit' to stop inspecting the subsystem.");
-        
-        String lastInput = "";
-        
-        while (!lastInput.toLowerCase().equals("quit")) {
-            
-            lastInput = console.readInputLine().strip();
-            console.println("(Implement the command interpreter for subsystem inspection)");
             
         }
         
