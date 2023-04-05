8613b7783393e42b6eb65832fed57fddffe71141
Gabriel Seaver <gabrielseaver9678@gmail.com>
2023-04-05 15:44:24 -0400

    SwerveTrajectoryController can correct for offsets from calculated trajectory over time

--- lib/app/src/main/java/claw/hardware/swerve/auto/SwerveMotionConstraints.java
+++ lib/app/src/main/java/claw/hardware/swerve/auto/SwerveMotionConstraints.java
@@ -2,8 +2,6 @@ package claw.hardware.swerve.auto;
 
 import claw.hardware.swerve.SwerveDriveHandler;
 import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.constraint.SwerveDriveKinematicsConstraint;
 
 /**
  * A class representing acceleration and velocity motion constraints on a {@link SwerveDriveHandler},
@@ -11,8 +9,6 @@ import edu.wpi.first.math.trajectory.constraint.SwerveDriveKinematicsConstraint;
  * calculations.
  */
 public class SwerveMotionConstraints {
-    
-    private final SwerveDriveHandler swerveDrive;
     
     public final double
         maxDriveSpeed,
@@ -30,43 +26,15 @@ public class SwerveMotionConstraints {
      * per second squared.
      */
     public SwerveMotionConstraints (
-        SwerveDriveHandler swerveDrive,
         double maxDriveSpeed,
         double maxDriveAcceleration,
         double maxRotationSpeed,
         double maxRotationAcceleration
     ) {
-        this.swerveDrive = swerveDrive;
         this.maxDriveSpeed =            maxDriveSpeed;
         this.maxDriveAcceleration =     maxDriveAcceleration;
         this.maxRotationSpeed =         maxRotationSpeed;
         this.maxRotationAcceleration =  maxRotationAcceleration;
-    }
-    
-    /**
-     * Get a {@link TrajectoryConfig} which limits the swerve drive to the provided acceleration and velocity limits,
-     * along with special constraint(s) to account for swerve drive kinematics.
-     * @return              A {@code TrajectoryConfig} for limiting the trajectory's acceleration and velocity.
-     */
-    public TrajectoryConfig getTrajectoryConfig () {
-        
-        // TODO: Apply rotational limits
-        
-        return
-            new TrajectoryConfig(
-                maxDriveSpeed,
-                maxDriveAcceleration
-            ).addConstraint(
-                // SwerveDriveKinematicsConstraint restricts the kinematics assuming that
-                // the swerve drive is actually turning according to the trajectory, even
-                // though this very well may not be the case, especially if we want to be able
-                // to use a trajectory that involves driving in a different direction than
-                // the robot is rotated in
-                new SwerveDriveKinematicsConstraint(
-                    swerveDrive.getKinematics(),
-                    swerveDrive.getMaxDriveSpeedMetersPerSec()
-                )
-            );
     }
     
 }
